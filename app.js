3ad9d19e64292ed2dff0b9537d6f28db78864fd7
Alex Zerah <alex.zerah@gmail.com>
2023-10-10 10:57:00 +0200

    404

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use(cors());
 
 app.use('/auth', authRouter);
 app.use('/api', verifyJWT() , indexRouter);
+app.use('*', (req, res, next) => {
+    res.status(404).json({message:'Not Found', method:req.method, url:req.url});
+})
 
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- controllers/querySearch.controller.js
+++ controllers/querySearch.controller.js
@@ -5,8 +5,8 @@ export const get = async (req, res) => {
     let role = req.body?.role;
 
     if (role === "admin") {
-        return res.status(200).json({message:'Code is YODA'});
+        return res.status(200).json({message:'Code is YODA1234'});
     }
 
-    res.status(400).json({message:`Vous n'avez pas les droits`});
+    res.status(400).json({message:`Vous n'avez pas les droits, vérifier le body de la requête`});
 }
